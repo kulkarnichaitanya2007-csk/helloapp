0bf477b39cfb2bbd933ca8e3a5bc333511454b89
kulkarnichaitanya2007-csk <kulkarnichaitanya2007@gmail.com>
2026-03-11 15:33:53 +0530

    third commit

---
+++ uc1.java
@@ -0,0 +1,17 @@
+/**
+ * OOPSBannerApp - UC1
+ * This application displays the literal text "OOPS" to the console.
+ * * @author YourName
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+
+    /**
+     * Main method: The entry point of the Java application.
+     * @param args Command line arguments
+     */
+    public static void main(String[] args) {
+        // UC1: Simple display of OOPS in the console
+        System.out.println("OOPS");
+    }
+}
