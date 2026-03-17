d11f7739373f7b0071da63a7a93c325b26c098ff
kulkarnichaitanya2007-csk <kulkarnichaitanya2007@gmail.com>
2026-03-17 13:11:18 +0530

    fifth commit

---
+++ uc3.java
@@ -0,0 +1,36 @@
+public class OOPSBannerUC3 {
+    public static void main(String[] args) {
+
+        // 7 lines for banner (each 9-char width per letter)
+
+        String line1 = String.join(" ",
+                " ***** ", " ***** ", " ****** ", " ****** ");
+        
+        String line2 = String.join(" ",
+                "*     *", "*     *", "*     * ", "*      ");
+        
+        String line3 = String.join(" ",
+                "*     *", "*     *", "******  ", " ***** ");
+        
+        String line4 = String.join(" ",
+                "*     *", "*     *", "*       ", "      *");
+        
+        String line5 = String.join(" ",
+                "*     *", "*     *", "*       ", "*     *");
+        
+        String line6 = String.join(" ",
+                "*     *", "*     *", "*       ", "*     *");
+        
+        String line7 = String.join(" ",
+                " ***** ", " ***** ", "*       ", " ***** ");
+
+        // Print banner
+        System.out.println(line1);
+        System.out.println(line2);
+        System.out.println(line3);
+        System.out.println(line4);
+        System.out.println(line5);
+        System.out.println(line6);
+        System.out.println(line7);
+    }
+}
